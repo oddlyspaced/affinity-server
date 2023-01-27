fd10eb21749b3e92bea447e5b23d1c9a41dfed07
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-27 20:06:38 +0530

    PhoneNumber: init data class

---
+++ src/main/kotlin/oddlyspaced/surge/data/PhoneNumber.kt
@@ -0,0 +1,9 @@
+package oddlyspaced.surge.data
+
+/**
+ * data class to store phone number representation
+ */
+data class PhoneNumber(
+    val countryCode: String,// country code
+    val phoneNumber: String,// phone number
+)
