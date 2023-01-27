576fac51ac0609dfb65929bc4580e9c51d52dd32
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-27 20:31:55 +0530

    Routing: register provider routing

--- src/main/kotlin/oddlyspaced/surge/plugins/Routing.kt
+++ src/main/kotlin/oddlyspaced/surge/plugins/Routing.kt
@@ -6,6 +6,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 import oddlyspaced.surge.routing.nodeRouting
+import oddlyspaced.surge.routing.providerRouting
 
 fun Application.configureRouting() {
 
@@ -14,6 +15,6 @@ fun Application.configureRouting() {
             call.respondText("Hello World!")
         }
 
-        nodeRouting()
+        providerRouting()
     }
 }
