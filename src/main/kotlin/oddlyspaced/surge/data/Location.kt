090cabf69e9d52e880220240cc4132a7c824d775
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-27 21:31:57 +0530

    Location: float -> double, add method for distance calc

--- src/main/kotlin/oddlyspaced/surge/data/Location.kt
+++ src/main/kotlin/oddlyspaced/surge/data/Location.kt
@@ -1,12 +1,26 @@
 package oddlyspaced.surge.data
 
 import kotlinx.serialization.Serializable
+import kotlin.math.*
 
 /**
  * data class to hold a location representation
  */
 @Serializable
 data class Location(
-    val lat: Float,
-    val lon: Float,
+    val lat: Double,
+    val lon: Double,
 )
+
+fun Location.distanceTo(point: Location): Double {
+    val dLat = deg2rad(abs(this.lat - point.lat))
+    val dLon = deg2rad(abs(this.lon - point.lon))
+    val a =
+        sin(dLat / 2.0) * sin(dLat / 2) + cos(deg2rad(this.lat)) * cos(deg2rad(point.lat)) * sin(dLon / 2) * sin(dLon / 2)
+    val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return 6371 * c
+}
+
+fun deg2rad(deg: Double): Double {
+    return deg * (Math.PI / 180.0)
+}
