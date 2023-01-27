1bfcb24da4a7f03abd5b70093d035c476431825e
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-27 21:32:16 +0530

    ProviderRoutes: add basic search method

--- src/main/kotlin/oddlyspaced/surge/routing/ProviderRoutes.kt
+++ src/main/kotlin/oddlyspaced/surge/routing/ProviderRoutes.kt
@@ -5,8 +5,11 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import oddlyspaced.surge.data.Location
 import oddlyspaced.surge.data.Provider
+import oddlyspaced.surge.data.distanceTo
 import oddlyspaced.surge.data.parameter.ProviderParameter
+import oddlyspaced.surge.data.parameter.SearchParameter
 import oddlyspaced.surge.data.providers
 import oddlyspaced.surge.data.parameter.toProviderInstance
 
@@ -25,6 +28,24 @@ fun Route.providerRouting() {
          */
         get("all") {
             call.respond(providers)
+        }
+        /**
+         * searches for providers
+         */
+        get("search") {
+            // add more params like name, phone, service
+            val params = call.receive<SearchParameter>()
+            val providedLocation = Location(params.searchFromLat, params.searchFromLon)
+            val searchResult = arrayListOf<Provider>()
+            providers.forEach { provider ->
+                if (provider.location.distanceTo(providedLocation) <= params.limitDistance) {
+                    searchResult.add(provider)
+                }
+                if (searchResult.size == params.limitCount) {
+                    call.respond(HttpStatusCode.OK, searchResult)
+                }
+            }
+            call.respond(HttpStatusCode.OK, searchResult)
         }
     }
 }
