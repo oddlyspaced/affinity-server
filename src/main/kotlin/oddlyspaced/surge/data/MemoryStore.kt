4b9ce57eab474c4ed9072745ed01ba0fe6b2ce6b
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-28 23:07:32 +0530

    MemoryStore: setup method for generating dummy data

--- src/main/kotlin/oddlyspaced/surge/data/MemoryStore.kt
+++ src/main/kotlin/oddlyspaced/surge/data/MemoryStore.kt
@@ -1,3 +1,41 @@
 package oddlyspaced.surge.data
 
+import io.github.serpro69.kfaker.Faker
+import kotlin.random.Random
+
 val providers = arrayListOf<Provider>()
+
+val sourcePoint = Location(26.882883,80.925594)
+
+fun generateData() {
+    val fake = Faker()
+    for (i in 0..25) {
+        providers.add(
+            Provider(
+                (providers.size + 1),
+                fake.name.name(),
+                PhoneNumber(
+                    fake.phoneNumber.countryCode.code(),
+                    fake.phoneNumber.phoneNumber()
+                ),
+                Location(
+                    sourcePoint.lat + ((0..100).random() / 1000.0),
+                    sourcePoint.lon + ((0..100).random() / 1000.0),
+                ),
+                (1..(1..10).random()).mapTo(arrayListOf()) {
+                    fake.adjective.positive()
+                }.flat()
+            )
+        )
+    }
+}
+
+fun ArrayList<String>.flat(): ArrayList<String> {
+    val flattened = arrayListOf<String>()
+    this.forEach {
+        if (!flattened.contains(it)) {
+            flattened.add(it)
+        }
+    }
+    return flattened
+}
