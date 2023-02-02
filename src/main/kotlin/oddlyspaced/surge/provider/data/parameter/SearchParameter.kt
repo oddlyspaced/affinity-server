220baa18979522e7440c490a9cca6384129c54cf
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-02 15:27:00 +0530

    SearchParameter: update params to include location and services

--- src/main/kotlin/oddlyspaced/surge/provider/data/parameter/SearchParameter.kt
+++ src/main/kotlin/oddlyspaced/surge/provider/data/parameter/SearchParameter.kt
@@ -1,11 +1,13 @@
 package oddlyspaced.surge.provider.data.parameter
 
 import kotlinx.serialization.Serializable
+import oddlyspaced.surge.provider.data.Location
 
 @Serializable
 class SearchParameter(
     val limitCount: Int = 10,
     val limitDistance: Int = 10,
-    val searchFromLat: Double,
-    val searchFromLon: Double,
+    val pickupPoint: Location,
+    val dropPoint: Location,
+    val filterServices: ArrayList<String>? = arrayListOf()
 )
