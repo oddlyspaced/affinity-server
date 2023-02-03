1dade07e126e7c8939374e4f03afed6fb1a46321
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-03 15:04:41 +0530

    Application: generate dummy data and print ip before starting server

--- src/main/kotlin/oddlyspaced/surge/Application.kt
+++ src/main/kotlin/oddlyspaced/surge/Application.kt
@@ -5,8 +5,11 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import oddlyspaced.surge.plugins.configureRouting
 import oddlyspaced.surge.plugins.configureSerialization
+import java.net.InetAddress
 
 fun main() {
+    println("Starting server at: ${InetAddress.getLocalHost().hostAddress}:4444")
+    generateData()
     embeddedServer(Netty, port = 4444, module = Application::module).start(wait = true)
 }
 
