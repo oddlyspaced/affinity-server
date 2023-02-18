f51901fea9261907ae0fe73167d50aef7a1d1b59
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-18 23:03:40 +0530

    Provider: set default id to -1 and add method to generate id at runtime

--- src/main/kotlin/oddlyspaced/surge/provider/data/Provider.kt
+++ src/main/kotlin/oddlyspaced/surge/provider/data/Provider.kt
@@ -1,17 +1,22 @@
 package oddlyspaced.surge.provider.data
 
 import kotlinx.serialization.Serializable
+import oddlyspaced.surge.providers
 
 /**
  * data class to hold the properties of a Provider
  */
 @Serializable
 data class Provider(
-    val id: Int, // unique id for provider
+    var id: Int = -1, // unique id for provider [-1 for default not initialized]
     val name: String, // name of provider
     val phone: PhoneNumber, // phone number of provider
     var location: Location,
     val services: ArrayList<String>, // tags of all the services the provider offers
     var areaServed: AreaServed, // area served by provider
     var isActive: Boolean,
-)
+) {
+    fun generateId() {
+        this.id = providers.size + 1
+    }
+}
