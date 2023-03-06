ef1875f7cfdbd2fd74686564ad8889c0bede1337
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-06 09:41:39 +0530

    Routing: add ping endpoint to check for server connectivity

--- src/main/kotlin/oddlyspaced/surge/plugins/Routing.kt
+++ src/main/kotlin/oddlyspaced/surge/plugins/Routing.kt
@@ -1,15 +1,17 @@
 package oddlyspaced.surge.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import oddlyspaced.surge.provider.data.ResponseError
 import oddlyspaced.surge.provider.routing.providerRouting
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/ping") {
+            call.respond(HttpStatusCode.OK, ResponseError("pong", false))
         }
 
         providerRouting()
