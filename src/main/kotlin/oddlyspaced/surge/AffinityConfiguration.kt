39973aea733645d0b1dfcb65d4864937200ff6cf
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-07 14:55:36 +0530

    AffinityConfiguration: create config class

---
+++ src/main/kotlin/oddlyspaced/surge/AffinityConfiguration.kt
@@ -0,0 +1,8 @@
+package oddlyspaced.surge
+
+class AffinityConfiguration {
+    companion object {
+        const val APPLICATION_PORT = 4444
+        const val GENERATE_DUMMY_DATA = false
+    }
+}
