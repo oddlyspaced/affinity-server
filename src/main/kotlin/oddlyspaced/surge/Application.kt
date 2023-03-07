2012f5c1c464874993574b12aa656fa58137ee74
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-07 15:54:12 +0530

    Application, LocalStorageManager: use logger

--- src/main/kotlin/oddlyspaced/surge/Application.kt
+++ src/main/kotlin/oddlyspaced/surge/Application.kt
@@ -3,16 +3,47 @@ package oddlyspaced.surge
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import oddlyspaced.surge.plugins.configureRouting
 import oddlyspaced.surge.plugins.configureSerialization
+import oddlyspaced.surge.util.LocalStorageManager
+import oddlyspaced.surge.util.Logger
 import java.net.InetAddress
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
 
 fun main() {
     println("Starting server at: ${InetAddress.getLocalHost().hostAddress}:${AffinityConfiguration.APPLICATION_PORT}")
     if (AffinityConfiguration.GENERATE_DUMMY_DATA) {
+        Logger.println("Generating Dummy Data as requested")
         generateData()
+        Logger.println("Generated ${providers.size} dummy providers")
     }
+    if (AffinityConfiguration.RESTORE_DATA_ON_BOOT) {
+        // restore data
+        Logger.println("Restoring data from storage as requested")
+        LocalStorageManager.loadProviderInfoFromStorage().let { loadedProviders ->
+            Logger.println("Loaded ${loadedProviders.size} providers from storage")
+            providers.addAll(loadedProviders)
+        }
+    }
+    if (AffinityConfiguration.SAVE_DATA_TO_STORAGE) {
+        Logger.println("Data Storage Polling requested at ${AffinityConfiguration.SAVE_DATA_INTERVAL} intervals")
+        pollDataStorage()
+    }
+    // server runs blocking the main thread
     embeddedServer(Netty, port = AffinityConfiguration.APPLICATION_PORT, module = Application::module).start(wait = true)
+}
+
+private fun pollDataStorage() {
+    CoroutineScope(Dispatchers.IO).launch {
+        while (true) {
+            delay(AffinityConfiguration.SAVE_DATA_INTERVAL)
+            LocalStorageManager.saveProviderInfoToStorage()
+        }
+    }
 }
 
 fun Application.module() {
--- src/main/kotlin/oddlyspaced/surge/util/LocalStorageManager.kt
+++ src/main/kotlin/oddlyspaced/surge/util/LocalStorageManager.kt
@@ -5,9 +5,6 @@ import com.google.gson.reflect.TypeToken
 import oddlyspaced.surge.provider.data.Provider
 import oddlyspaced.surge.providers
 import java.io.File
-import java.time.Instant
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 object LocalStorageManager {
     fun saveProviderInfoToStorage() {
@@ -19,13 +16,14 @@ object LocalStorageManager {
                 }
                 if (file.createNewFile()) {
                     file.writeText(Gson().toJson(data))
-                    println("${getCurrentTimeStamp()}: ${providers.size} written to storage.")
+                    Logger.println("${providers.size} written to storage.")
                 }
             }
         }
         catch (e: Exception) {
             e.printStackTrace()
-            println("${getCurrentTimeStamp()}: Exception error while trying to write providers to storage.")
+            Logger.println("Exception error while trying to write providers to storage.")
+            Logger.println(e.stackTraceToString())
         }
     }
     fun loadProviderInfoFromStorage(): ArrayList<Provider> {
@@ -42,15 +40,9 @@ object LocalStorageManager {
         }
         catch (e: Exception) {
             e.printStackTrace()
-            println("${getCurrentTimeStamp()}: Exception error while trying to write providers to storage.")
+            Logger.println("Exception error while trying to write providers to storage.")
+            Logger.println(e.stackTraceToString())
             return arrayListOf()
         }
-    }
-
-    private fun getCurrentTimeStamp(): String? {
-        return DateTimeFormatter
-            .ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS")
-            .withZone(ZoneOffset.UTC)
-            .format(Instant.now())
     }
 }
