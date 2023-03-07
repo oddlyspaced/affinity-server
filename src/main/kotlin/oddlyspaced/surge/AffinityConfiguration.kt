e1f364ceb2ff8c1e703a065020dd614e57eef4ff
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-07 15:11:39 +0530

    AffinityConfiguration: add config variables related to data storage

--- src/main/kotlin/oddlyspaced/surge/AffinityConfiguration.kt
+++ src/main/kotlin/oddlyspaced/surge/AffinityConfiguration.kt
@@ -4,5 +4,8 @@ class AffinityConfiguration {
     companion object {
         const val APPLICATION_PORT = 4444
         const val GENERATE_DUMMY_DATA = false
+        const val RESTORE_DATA_ON_BOOT = false
+        const val SAVE_DATA_TO_STORAGE = true
+        const val SAVE_DATA_INTERVAL = 1000 * 10L // ms time
     }
 }
