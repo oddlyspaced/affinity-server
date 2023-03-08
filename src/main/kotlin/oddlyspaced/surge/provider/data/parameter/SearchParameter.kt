c6425100176db921d60f2204afde14333ce525e3
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-08 16:08:58 +0530

    SearchParameter: update schema

--- src/main/kotlin/oddlyspaced/surge/provider/data/parameter/SearchParameter.kt
+++ src/main/kotlin/oddlyspaced/surge/provider/data/parameter/SearchParameter.kt
@@ -2,13 +2,13 @@ package oddlyspaced.surge.provider.data.parameter
 
 import kotlinx.serialization.Serializable
 import oddlyspaced.surge.provider.data.Location
+import oddlyspaced.surge.provider.data.Service
 
 @Serializable
 class SearchParameter(
     val limitCount: Int = 10,
     val limitDistance: Int = 10,
-    val pickupLat: Double,
-    val pickupLon: Double,
-    val dropLat: Double,
-    val dropLon: Double,
+    val serviceFilters: ArrayList<String>,
+    val pickupLocation: Location,
+    val dropLocation: Location,
 )
