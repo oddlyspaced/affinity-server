8b04ce59435dc88194f5301992e4c6b101d96f39
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-08 09:36:11 +0530

    AffinityConfiguration: document class

--- src/main/kotlin/oddlyspaced/surge/AffinityConfiguration.kt
+++ src/main/kotlin/oddlyspaced/surge/AffinityConfiguration.kt
@@ -1,11 +1,20 @@
 package oddlyspaced.surge
 
+/**
+ * Configuration class for server
+ * todo: make this class load from a config file or command line params
+ */
 class AffinityConfiguration {
     companion object {
-        const val APPLICATION_PORT = 4444
-        const val GENERATE_DUMMY_DATA = false
+        // netty server port
+        const val APPLICATION_PORT = 5030
+        // should dummy data be generated
+        const val GENERATE_DUMMY_DATA = true
+        // should data be restored from providers.json on first start
         const val RESTORE_DATA_ON_BOOT = false
+        // should data be stored to providers.json
         const val SAVE_DATA_TO_STORAGE = true
-        const val SAVE_DATA_INTERVAL = 1000 * 10L // ms time
+        // interval between write to providers.json file
+        const val SAVE_DATA_INTERVAL = 1000 * 60 * 60L // ms time
     }
 }
